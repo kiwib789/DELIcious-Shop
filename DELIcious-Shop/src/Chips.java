30b93b62f199648e026bd379c907aff3f26bf7c7
Keely Lowder <keelybowman@icloud.com>
2024-11-13 07:46:41 -0500

    created chips class

--- DELIcious-Shop/src/Chips.java
+++ DELIcious-Shop/src/Chips.java
@@ -1,2 +1,10 @@
-public class Chips {
+public class Chips extends Product {
+    public Chips(String name, double price) {
+        super(name, price);
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
 }
